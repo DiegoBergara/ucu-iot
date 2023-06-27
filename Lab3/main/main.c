e317c33d336ebd67d7ba1db948ef88821a074371
Diego Bergara <diegobergaraj@gmail.com>
2023-06-27 18:56:39 -0300

    Lab 3 Ready

--- Lab3/main/main.c
+++ Lab3/main/main.c
@@ -2,63 +2,118 @@
 #include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_system.h"
-#include "esp_console.h"
-#include "esp_vfs_dev.h"
-#include "esp_vfs_fat.h"
+#include "freertos/queue.h"
+#include "freertos/timers.h" 
 #include "driver/uart.h"
 
-#define MAX_LINE_LENGTH 256
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "esp_log.h"
 
-void app_main(void)
+#include "led_control.h"
+#include "led_strip.h"
+
+#define LED_RED '0'
+#define LED_GREEN '1'
+#define LED_BLUE '2'
+
+#define ECHO_TEST_TXD (GPIO_NUM_42)
+#define ECHO_TEST_RXD (GPIO_NUM_44)
+#define ECHO_TEST_RTS (UART_PIN_NO_CHANGE)
+#define ECHO_TEST_CTS (UART_PIN_NO_CHANGE)
+
+#define ECHO_UART_PORT_NUM      (0)
+#define ECHO_UART_BAUD_RATE     (115200)
+#define ECHO_TASK_STACK_SIZE    (5120)
+
+static const char *TAG = "UART TEST";
+
+#define BUF_SIZE (1024)
+
+QueueHandle_t queue;
+
+typedef struct {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+} RGB_t;
+
+
+void timer_callback(xTimerHandle xTimer) {
+    RGB_t *color = (RGB_t*)pvTimerGetTimerID(xTimer);
+    xQueueSend(queue, color, portMAX_DELAY);
+    ESP_LOGI(TAG, "Enviar color luego de: %hhu, %hhu, %hhu", color->r, color->g, color->b);
+    free(color); 
+}
+
+void TaskC(void* pvParameters) {
+    RGB_t color;
+    while(1) {
+        ESP_LOGI("LEEED", "Recv RGB: %hhu, %hhu, %hhu", color.r, color.g, color.b);
+        if (xQueueReceive(queue, &color, portMAX_DELAY)) {
+            set_color(color.r, color.g, color.b);
+        }
+    }
+}
+
+
+static void TaskB(void *arg)
 {
-    // Configurar UART para la consola
     uart_config_t uart_config = {
-        .baud_rate = CONFIG_ESP_CONSOLE_UART_BAUDRATE,
+        .baud_rate = ECHO_UART_BAUD_RATE,
         .data_bits = UART_DATA_8_BITS,
-        .parity = UART_PARITY_DISABLE,
+        .parity    = UART_PARITY_DISABLE,
         .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_APB,
     };
+    int intr_alloc_flags = 0;
 
-    // Instalar el controlador UART y configurarlo
-    ESP_ERROR_CHECK(uart_driver_install(CONFIG_ESP_CONSOLE_UART_NUM, 256, 0, 0, NULL, 0));
-    ESP_ERROR_CHECK(uart_param_config(CONFIG_ESP_CONSOLE_UART_NUM, &uart_config));
+#if CONFIG_UART_ISR_IN_IRAM
+    intr_alloc_flags = ESP_INTR_FLAG_IRAM;
+#endif
+    ESP_ERROR_CHECK(uart_param_config(ECHO_UART_PORT_NUM, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(ECHO_UART_PORT_NUM, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS));
+    ESP_ERROR_CHECK(uart_driver_install(ECHO_UART_PORT_NUM, BUF_SIZE * 2, 0, 0, NULL, intr_alloc_flags));
 
-    // Configurar el sistema de archivos VFS
-    esp_vfs_dev_uart_use_driver(CONFIG_ESP_CONSOLE_UART_NUM);
-    uart_set_line_inverse(CONFIG_ESP_CONSOLE_UART_NUM, UART_SIGNAL_TXD_INV);
-
-    // Inicializar la consola
-    esp_console_config_t console_config = {
-        .max_cmdline_args = 8,
-        .max_cmdline_length = 256,
-    };
-    ESP_ERROR_CHECK(esp_console_init(&console_config));
-
-    // Configurar el sistema de archivos FAT para la consola
-    esp_vfs_fat_mount_config_t mount_config = {
-        .format_if_mount_failed = true,
-        .max_files = 4,
-        .allocation_unit_size = CONFIG_WL_SECTOR_SIZE
-    };
-    wl_handle_t s_wl_handle; // Declaración de la variable s_wl_handle
-    ESP_ERROR_CHECK(esp_vfs_fat_spiflash_mount("/fat", "storage", &mount_config, &s_wl_handle));
-
-    printf("Texto de ejemplo\n");
+    // Configure a temporary buffer for the incoming data
+    uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
 
     while (1) {
-        // Leer línea de texto desde la consola
-        char line[MAX_LINE_LENGTH];
-        fgets(line, sizeof(line), stdin);
+        // Read data from the UART
+        int len = uart_read_bytes(ECHO_UART_PORT_NUM, data, (BUF_SIZE - 1), 20 / portTICK_RATE_MS);
+        data[len] = '\0';
+        RGB_t color;
 
-        // Eliminar el carácter de nueva línea si está presente
-        char *newline = strchr(line, '\n');
-        if (newline != NULL) {
-            *newline = '\0';
+        if (len) {
+            uint32_t delay;
+            RGB_t *color = malloc(sizeof(RGB_t));
+            if(sscanf((char*)data, "%hhu,%hhu,%hhu,%u", &(color->r), &(color->g), &(color->b), &delay) == 4) {
+                if (delay == 0) {
+                    // Si el delay es cero, enviar inmediatamente
+                    xQueueSend(queue, color, portMAX_DELAY);
+                    ESP_LOGI(TAG, "Recv RGB: %hhu, %hhu, %hhu", color->r, color->g, color->b);
+                    free(color);
+                 } else {
+                    // Si el delay es distinto de cero, usar timer
+                    xTimerHandle timer = xTimerCreate("ColorTimer", pdMS_TO_TICKS(delay), pdFALSE, color, timer_callback);
+                    xTimerStart(timer, 0);
+                     ESP_LOGI(TAG, "El tiempo comienza con delay: %u", delay);
+                }
+            } else {
+                ESP_LOGE(TAG, "Error al leer rgb y timer");
+                free(color);
+            }
         }
-
-        // Realizar acciones con el texto recibido
-        printf("Texto recibido: %s\n", line);
     }
+}
+
+void app_main() {
+    queue = xQueueCreate(10, sizeof(RGB_t));
+    led_init();
+    // Conectarse al puerto uart via putty
+    // Serial Line: (donde este conectada la placa)
+    // Speed: 115200
+    xTaskCreate(TaskB, "UART TASK", ECHO_TASK_STACK_SIZE, NULL, 10, NULL);
+    xTaskCreate(TaskC, "LED Task", 2048, NULL, 1, NULL);
 }

557ca60689acef0c9cd5db00fb84ff42a3fabfa1
Felipe <felipe.cabrera@correo.ucu.edu.uy>
2023-07-03 15:37:59 -0300

    SNTP y Components en Chimera

--- chimera/main/main.c
+++ chimera/main/main.c
@@ -18,6 +18,19 @@
 #include "esp_err.h"
 #include "esp_adc_cal.h"
 
+#include "sta-AP.h"
+#include "server.h"
+
+#include "esp_sntp.h"
+#include "lwip/apps/sntp.h"
+
+#include "lwip/opt.h"
+#include "lwip/udp.h"
+#include "lwip/dns.h"
+#include "lwip/ip_addr.h"
+#include "lwip/pbuf.h"
+#include "lwip/dhcp.h"
+
 
 #define BUFFER_SIZE 100
 #define NVS_NAMESPACE "storage"
@@ -113,6 +126,30 @@ void app_main(void)
 {
     ESP_LOGI(TAG, "Hola Mundo");
     // init_sensor();
+    time_t now;
+    struct tm timeinfo;
+    //1 para STA o 2 para AP 
+    setMode(1);
+    server_init();
+    setenv("TZ","UTC+3",1);
+    tzset();
+    sntp_setoperatingmode(SNTP_OPMODE_POLL);
+    sntp_setservername(0, "pool.ntp.org");
+    sntp_init();
+    while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET) {
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+    time(&now);
+    localtime_r(&now, &timeinfo);
+    
+    // Print the current time (optional)
+    char strftime_buf[64];
+    strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
+    ESP_LOGI(TAG, "Current time: %s", strftime_buf);
+
+
+    init_sensor();
+    get_sensor_value();
 
     esp_err_t err = nvs_flash_init();
     if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
